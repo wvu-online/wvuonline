b6811f0355633271cb11fa4c8506de045c82c0d8
Gregg Shanefelt <grshane@gmail.com>
2018-08-02 23:45:52 -0400

    Styles for responsive hero images

---
+++ web/themes/custom/wvuonline2018/components/_patterns/03-organisms/hero/hero.js
@@ -0,0 +1,43 @@
+Drupal.behaviors.responsiveHero = {
+  attach: function (context, settings) {
+
+// (function () { // REMOVE IF DRUPAL
+
+    'use strict';
+
+    // Your JS Code Here
+    class ResponsiveBackgroundImage {
+
+      constructor(element) {
+        this.element = element;
+        this.img = element.querySelector('img');
+        this.src = '';
+
+        this.img.addEventListener('load', () => {
+          this.update();
+        });
+
+        if (this.img.complete) {
+          this.update();
+        }
+      }
+
+      update() {
+        let src = typeof this.img.currentSrc !== 'undefined' ? this.img.currentSrc : this.img.src;
+        if (this.src !== src) {
+          this.src = src;
+          this.element.style.backgroundImage = 'url("' + this.src + '")';
+
+        }
+      }
+    }
+
+    let elements = document.querySelectorAll('[data-responsive-background-image]');
+    for (let i = 0; i < elements.length; i++) {
+      new ResponsiveBackgroundImage(elements[i]);
+    }
+// })(); // REMOVE IF DRUPAL
+
+// UNCOMMENT IF DRUPAL
+  }
+};
